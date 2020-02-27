d2b81e8ed55caabd79b61d32687bad5e00626d46
dandelior <jberrioscifuentes@gmail.com>
2020-02-27 17:27:29 -0300

    modales

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,33 @@
       $(this).toggleClass('is-active');
       $('.buttons').toggleClass('open-buttons', 100)
     });
+
+    $('.button-plan-info').click(function(){
+      $('.modal-obtener').addClass('show-modal');
+      return false;
+    });
+    $('.button-plan-prueba').click(function(){
+      $('.modal-contratar').addClass('show-modal');
+      return false;
+    });
+    $('.modal .x').click(function(){
+      $('.modal').removeClass('show-modal');
+      return false;
+    });
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 500) {
+        $('header').addClass('on-down');
+      } else {
+        $('header').removeClass('on-down');
+      }
+        
+      });
+
+    AOS.init({
+      once: true
+    });
   
 } )( jQuery );
